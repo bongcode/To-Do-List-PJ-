f80f56d3620a619be17a3fc09d02be7fb6772a6f
baonggoo <thdqhdrms921105@gmail.com>
2022-02-06 16:11:45 +0900

    final

--- main.js
+++ main.js
@@ -5,110 +5,119 @@
 // 진행중 끝남, 언더바가 이동한다
 // 끝난 탭은, 끝난 아이탭만, 진행중인 아이템만, 전체는 전체아이템으로 돌아옴
 
-let taskInput = document.getElementById("task-input");
-let addButton = document.getElementById("add-button");
-let tabs = document.querySelectorAll(".task-tabs div")
-let mode = "all"
-let filterList = []
-let taskList = []
+let userInput = document.querySelector(".task-input");
+let addButton = document.querySelector(".button-add");
+let tabs = document.querySelectorAll(".tab-type div");
+let underLine = document.getElementById("tab-underline");
+let taskList = [];
+let selectedMenu = "tab-all";
+let filteredList = [];
 
-
-addButton.addEventListener("click", addTask);
-
-for (let i=1; i<tabs.length; i++){
-    tabs[i].addEventListener("click", function(event){filter(event)})
+addButton.addEventListener("mousedown", addTask);
+userInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
 }
 
-console.log(tabs)
+function addTask() {
+  let taskValue = userInput.value;
+  let task = {
+    content: taskValue,
+    isComplete: false,
+    id: randomIDGenerator(),
+  };
 
-
-function addTask(){
-    let task = {
-        id:randomIDgenerate(),
-        taskContent: taskInput.value,
-        isComplete: false
-    }
-    taskList.push(task);
-    render()
-    console.log(taskList)
+  taskList.push(task);
+  userInput.value = "";
+  render();
 }
 
-function render(){
-    let list = []
-    if(mode == "all"){
-        list = taskList
-    }else if(mode == "ongoing" || mode == "done"){
-        list = filterList
-    }
+function render() {
+  let result = "";
+  list = [];
+  if (selectedMenu === "tab-all") {
+    list = taskList;
+  } else {
+    list = filteredList;
+  }
 
-    let resultHTML = '';
-    for(let i=0;i<list.length;i++){
-        if(list[i].isComplete == true){
-            resultHTML += `<div class="task">
-            <div class="task-done" > ${list[i].taskContent} </div> 
-            <div>
-                <button onclick="toggleComplete('${list[i].id}')">Check</button>
-                <button onclick="deleteTask('${list[i].id}')">Delete</button>
-            </div> 
-        </div>`
-        } else{
-        resultHTML += `<div class="task">
-            <div> ${list[i].taskContent} </div> 
-            <div>
-                <button onclick="toggleComplete('${list[i].id}')">Check</button>
-                <button onclick="deleteTask('${list[i].id}')">Delete</button>
-            </div> 
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
+      result += `<div class="task task-done" id="${list[i].id}">
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
         </div>`;
-        }
+    } else {
+      result += `<div class="task" id="${list[i].id}" >
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
     }
+  }
 
-    document.getElementById("task-board").innerHTML = resultHTML;
+  document.getElementById("task-board").innerHTML = result;
 }
 
-function toggleComplete(id){
-    for(let i=0;i<taskList.length;i++){
-        if(taskList[i].id == id){
-            taskList[i].isComplete = !taskList[i].isComplete;
-            break;
-        }
+function toggleDone(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
     }
-    render()
-    console.log(taskList)
+  }
+  filter();
 }
 
-function deleteTask(id){
-    for(let i=0; i<taskList.length;i++){
-        if(taskList[i].id == id){
-            taskList.splice(i,1)
-            break;
-        }
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
     }
-    render()
+  }
+
+  filter();
+}
+function filter(e) {
+  if (e) {
+    selectedMenu = e.target.id;
+    underLine.style.width = e.target.offsetWidth + "px";
+    underLine.style.left = e.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + "px";
+  }
+
+  filteredList = [];
+  if (selectedMenu === "tab-not-done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filteredList.push(taskList[i]);
+      }
+    }
+  } else if (selectedMenu === "tab-done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filteredList.push(taskList[i]);
+      }
+    }
+  }
+  render();
 }
 
-function filter(event){
-    mode = event.target.id
-    filterList = []
-    console.log("filter 클릭됨!", event.target.id);
-    if(mode == "all"){
-        render();
-    }else if(mode == "ongoing"){
-        for(let i=0; i<taskList.length; i++){
-            if(taskList[i].isComplete == false){
-                filterList.push(taskList[i]);
-            }
-        }
-        render()
-    } else if(mode == "done"){
-        for(let i=0; i<taskList.length; i++){
-            if(taskList[i].isComplete == true){
-                filterList.push(taskList[i])
-            }
-        }
-        render();
-    }
-} 
-
-function randomIDgenerate(){
-    return '_' + Math.random().toString(36).substr(2, 16);
+function randomIDGenerator() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
